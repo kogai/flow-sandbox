8f5a34934a5b5376a96577950ffe7204a263e91d
shinchi kogai <kogai0121@gmail.com>
2017-11-18 15:44:45 +0900

    ExpectError

--- mycode.js
+++ mycode.js
@@ -1,14 +1,13 @@
+// @flow
+
 type P = {
   a: string,
   b: number,
 };
 
-({
-  a: "",
-  b: 0,
-}: $ObjMap<P, <V>(V) => V>);
+type M = $ObjMap<P, <V>(V) => () => V>;
 
 ({
   a: () => "",
   b: () => "0",
-}: $ObjMap<P, <V>(V) => () => V>);
+}: M);
